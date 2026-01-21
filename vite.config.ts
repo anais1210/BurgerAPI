aa7ce2ec8a287777dba2e7540bede3547c760b62
anais1210 <anais.zhang12@gmail.com>
2026-01-21 14:36:22 +1030

    build base route

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/BurgerAPI/", // EXACTEMENT le nom du repo GitHub
+  base: "BurgerAPI", // EXACTEMENT le nom du repo GitHub
 });
